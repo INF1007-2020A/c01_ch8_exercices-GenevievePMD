3ba23dcd017cba2b42cfb3ed2054e6a877607bc0
Genevieve Pelletier-Mc Duff <genevieve.pelletier-mc-duff@polymtl.ca>
2020-12-17 19:01:02 -0500

    Exercices refaits au complet

--- exercice.py
+++ exercice.py
@@ -7,6 +7,140 @@ import json     # fichiers json
 import string
 
 # TODO: Définissez vos fonction ici
+# Prise 2 - Exercice 1
+def comparer_deux_fichiers(nom_fichier1 : str, nom_fichier2:str):
+    with open(nom_fichier1, "r", encoding="utf-8") as fichier1, open(nom_fichier2, "r", encoding="utf-8") as fichier2:
+        est_pareille = True
+
+        while est_pareille:
+            a = fichier1.read(1)
+            b = fichier2.read(1)
+            est_pareille = a == b
+
+        return fichier1.tell(), a, b
+
+''' 2. Écrire un programme qui recopie un fichier texte en triplant tous les espaces entre les mots. 
+(vous pouvez ouvrir deux fichiers avec l’instruction with). '''
+def tripler_espace(nom_fichier1: str, nouveau_fichier: str):
+    with open(nom_fichier1, "r", encoding="utf-8") as fichier1, open(nouveau_fichier, "w", encoding="utf-8") as nouveau_fichier:
+        nouveau_fichier.write(fichier1.read().replace(' ', '   '))
+
+
+'''3. Écrire un programme qui lit chaque ligne d’un fichier notes.txt (chaque ligne contient une note en pourcentage) 
+et qui réécrit, dans un nouveau fichier, les notes avec, à coté, les mentions « A », « B », etc. en 
+fonction d’un tableau de correspondance fourni. '''
+def correspondance_notes(fichier_notes: str, nouveau_fichier:str):
+    correspondances = {0: "E", 20: "F", 40: "D", 50: "C", 70: "B", 80: "A"}
+    note_precedente = []
+
+    with open(fichier_notes, "r", encoding="utf-8") as f_notes, open(nouveau_fichier, "w", encoding="utf-8") as f_nouveau:
+        for ligne in f_notes:
+            note = float(ligne)
+            for note_bareme, lettre in correspondances.items():
+                if note <= note_bareme:
+                    f_nouveau.write(f'{note}% : {note_precedente[1]} \n')
+                    break
+                if note > 80:
+                    if note > 90:
+                        f_nouveau.write(f'{note}% : A* \n')
+                    else:
+                        f_nouveau.write(f'{note}% : A \n')
+                    break
+                note_precedente = [note_bareme, lettre]
+
+''' 4. Reprenez l’exercice du livre de recettes et créer une base de données dans un fichier qui permet d’ajouter, 
+    modifier, supprimer des recettes. Vous êtes libre de choisir le type de format de fichier.
+'''
+def creation_livre_recette(fichier_recette: str):
+    livre_recettes = {}
+
+    reponse = input("Voulez-vous écrire une nouvelle recette? (oui/non) ")
+
+    while reponse == 'oui':
+        ingredient = input("Quel est le premier ingrédient? ")
+        recette = [ingredient]
+        reponse_ingredient = input("Voulez-vous entrer un nouvel ingrédient? (oui/non) ")
+        while reponse_ingredient == 'oui':
+            ingredient = input("Quel est le nouvel ingrédient? ")
+            recette.append(ingredient)
+            reponse_ingredient = input("Voulez-vous entrer un nouvel ingrédient? (oui/non) ")
+
+        nom_recette = input("Merci d'avoir partagé cette recette avec nous. Quel est son nom? ")
+        livre_recettes[nom_recette] = recette
+        reponse = input("Voulez-vous écrire une nouvelle recette? (oui/non) ")
+    with open(fichier_recette, 'a', encoding="utf-8") as f_recette:
+        json.dump(livre_recettes, f_recette, indent=4)
+
+'''
+5. Écrire un programme qui lit un fichier texte [exemple.txt](exemple.txt) et retourne une liste de tous 
+les nombres présents dans le fichier, en ordre croissant. 
+'''
+def retourner_nombre(fichier:str):
+    with open(fichier, 'r', encoding="utf-8") as f:
+        nombres = []
+        for ligne in f:
+            liste_mots = ligne.split()
+            for mot in liste_mots:
+                if mot.isdigit():
+                    nombres.append(int(mot))
+
+    return sorted(nombres)
+
+'''
+6. Écrire un programme qui lit un fichier et qui recopie une ligne sur deux dans un autre fichier.
+'''
+def lire_une_ligne_sur_deux(nom_fichier: str):
+    with open(nom_fichier, 'r', encoding="utf-8") as f:
+        texte = []
+        for ligne in f:
+            texte.append(ligne.replace('\n', ''))
+
+        for i in range(0, len(texte), 2):
+            print(texte[i])
+
+
+if __name__ == '__main__':
+    nom_fichier1 = 'fichiertexte1.txt'
+    nom_fichier2 = 'fichiertexte2.txt'
+    nouveau_fichier = 'nouveau_fichier2.txt'
+    fichier_notes = 'notes.txt'
+    fichier_notes_resultats = 'notes_resultats.txt'
+    fichier_recette = "livre_recettes.json"
+    fichier_exemple = 'exemple.txt'
+
+    # EX1
+    print(comparer_deux_fichiers(nom_fichier1, nom_fichier2))
+
+    # EX2
+    tripler_espace(nom_fichier1, nouveau_fichier)
+
+    # EX3
+    correspondance_notes(fichier_notes, fichier_notes_resultats)
+
+    # EX4
+    #creation_livre_recette(fichier_recette)
+
+    # EX5
+    print(retourner_nombre(fichier_exemple))
+
+    # EX6
+    lire_une_ligne_sur_deux(fichier_exemple)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+### ----------------------------------------------------------------------- #
+'''
 # Exercice 1
 def comparer_fichiers(nom_fichier_1 : str, nom_fichier_2 : str):
     with open(nom_fichier_1, "r") as f1, open(nom_fichier_2, "r") as f2:
@@ -17,11 +151,11 @@ def comparer_fichiers(nom_fichier_1 : str, nom_fichier_2 : str):
             b = f2.read(1)
 
             pareille = a == b
-        '''
+       
         # Équivalent
-        if a!=b
-            pareille = False
-         '''
+        #if a!=b
+        #    pareille = False
+        
         return -1 if pareille else f1.tell()
 
 # Exercice 2
@@ -122,4 +256,4 @@ if __name__ == '__main__':
     print(trouver_nombre_et_ordonner("exemple.txt"))    # Ma méthode
     print(nombre_croissant("exemple.txt")) # Méthode 2 - plus vite
 
-    pass
+'''
